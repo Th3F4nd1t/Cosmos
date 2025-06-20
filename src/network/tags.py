d91ffd1ef5fcfaa3288eca3fa177423b00bbafb2
Th3F4nd1t <indephisive@gmail.com>
2025-06-20 16:47:45 -0400

    Example code in tags.py

--- src/network/tags.py
+++ src/network/tags.py
@@ -1,1 +1,26 @@
 # this file is meant for tags
+from enum import Enum
+
+class Tags(Enum):
+  WPILIB_VERSION = WPILibVersion
+  ...
+
+class WPILibVersion:
+  class Status(Enum):
+    BAD = "<bad>"
+    GOOD = "<good>"
+    PREFERRED = "<preferred>"
+
+  def __init__(self):
+    self.status:str = None
+    self.version:str = None
+  ...
+
+
+"""
+In ds_net.py call from the Tags enum like this:
+
+tag = Tags.WPILIB_VERSION()
+tag.status = WPILibVersion.Status.GOOD
+...
+"""
