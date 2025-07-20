538cf2ab944c8b74526070b3acd88aa10353b835
Fandit <indephisive@gmail.com>
2025-07-20 16:52:03 -0500

    added some basic skeleton to the setup bash scripts... will do more work after I get the main2.py file done and have more focus on this

--- bin/cosmos.sh
+++ bin/cosmos.sh
@@ -1,1 +1,7 @@
-echo "tbd..."
+echo "wip, don't work cuz no venv being setup by script. will add this to setup.sh and check a is_setup.tx file in the bin to see if it is able to use venv otherwise it'll stop cuz lowk idc"
+
+
+
+if ! (pip3 install -r ../requirements.txt > /dev/null 2>&1 || pip install -r ../requirements.txt > /dev/null 2>&1);
+	then echo "Something went wrong with installing requirements. "; fi
+
