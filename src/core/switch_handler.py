036e046d451ce407c4b8d20b01230c6eda8d4681
Fandit <indephisive@gmail.com>
2025-07-21 11:13:19 -0500

    added emits

--- src/core/switch_handler.py
+++ src/core/switch_handler.py
@@ -1,13 +1,19 @@
 from enum import Enum
 import logging
 from netmiko import ConnectHandler
+from src.core.eventbus.events import GeneralEvent, SwitchEvent
 
-# logger = logging.getLogger("switch_handler")
+
+class SwitchConfigMetadata:
+    def __init__(self, description: str=None, version: str=None, author: str=None):
+        self.description = description
+        self.version = version
+        self.author = author
 
 class SwitchConfig:
-    def __init__(self, config: dict, metadata: dict):
+    def __init__(self, config: dict, metadata: SwitchConfigMetadata):
         self.config = config
-        self.metadata = metadata
+        self.metadata: SwitchConfigMetadata = metadata
 
 
 class SwitchType(Enum):
@@ -20,7 +26,12 @@ class SwitchType(Enum):
 
 
 class SwitchHandler:
-    def __init__(self, name: str, ip: str, username: str, password: str, switch_type: SwitchType):
+    def __init__(self, name: str, ip: str, username: str, password: str, switch_type: SwitchType, fms=None):
+        if fms is None:
+            raise ValueError("FMS instance must be provided")
+        else:
+            self.fms = fms
+
         self.name = name
         self.ip = ip
         self.username = username
@@ -32,7 +43,7 @@ class SwitchHandler:
     def add_config(self, config: SwitchConfig):
         self.config = config.config
         self.metadata = config.metadata
-        logger.log(f"Config added.")
+        self.fms.emit(GeneralEvent.INFO, {"message": f"Config added to switch {self.name}"})
 
     def push_config(self):
         if self.config:
@@ -45,17 +56,15 @@ class SwitchHandler:
                 )
 
                 output = conn.send_config_set(self.config)
-                logger.info(f"Config pushed to {self.name}.")
-                logger.log(output)
+                self.fms.emit(SwitchEvent.CONFIGURED, {"switch": f"{self.name}", "config_desc": self.metadata.description if self.metadata else "No description"})
                 conn.save_config()
                 conn.disconnect()
 
             except Exception as e:
-
-                logger.error(f"Failed to configure {self.name}: {e}")
+                self.fms.emit(SwitchEvent.ERROR, {"switch": f"{self.name}", "error": f"Failed to configure switch {self.name}: {str(e)}"})
 
             conn.send_config_set(self.config)
 
-            logger.log(f"Config pushed.")
+            self.fms.emit(GeneralEvent.INFO, {"message": f"Config pushed to switch {self.name}"})
         else:
-            logger.log(f"No config to push.")
+            self.fms.emit(GeneralEvent.WARNING, {"message": f"No config to push to switch {self.name}"})
